1b178663a21c75a8c04cf11e882978aa313774ab
Quirin Langer <langer@ris-development.de>
2021-01-25 21:20:03 +0100

    added the Germany graph and a little scale

--- js/covid.js
+++ js/covid.js
@@ -43,10 +43,10 @@ export function getCovid(){
     req3.onload = () =>{
         ans = req3.response;
         let arr = [];
-        let days = 14;
-        let min = 0;
+        let days = 30;
         let ID_BY = 9;
         let daily = [];
+        let daily_DE = Array(days+1).fill(0);
         ans.split("\n").forEach((line, i) => {
             //converts arr from string to int if possible (names are not convertable)
             arr[i] = line.split(",").map((x)=>isNaN(+x)?x:+x); 
@@ -61,7 +61,6 @@ export function getCovid(){
         daily[ID_BY].forEach(i => {
             max_BY = i>max_BY?i:max_BY;
         });
-        
         let max_DE = 0;
         let sum = 0;
         for(let i=3; i < daily[1].length; i++){
@@ -71,10 +70,21 @@ export function getCovid(){
             max_DE = sum>max_DE?sum:max_DE;
             sum = 0;
         }
+        document.querySelector('#max-bav').textContent = `${max_BY} / Tag`;
+        document.querySelector('#max-de').textContent = `${max_DE} / Tag`;
 
         //shortens the array (+1 because we calc the current day - the day before)
         daily.forEach((x, y)=>{
             daily[y] = daily[y].slice(daily[y].length-(days+1)); 
+        });
+        daily = daily.slice(0,17);
+
+        daily.forEach((i, j) =>{
+            daily[1].forEach((x, y)=>{
+                if (daily[j][y] > 0 && !isNaN(daily[j][y])){
+                    daily_DE[y] += daily[j][y];
+                }
+            });
         });
 
         let lookup = arr[0].slice(arr[0].length-(days+1)); 
@@ -85,8 +95,8 @@ export function getCovid(){
         //lookup is an array of the same lenght as daily[i] and has the corresponding dates as a lookup
 
 
-        // Let the drawing begin 😅
-        let canvas1 = document.getElementById("bav");
+    // Let the drawing begin (bavaria)
+        var canvas1 = document.getElementById("bav");
         let c1height = canvas1.clientHeight;
         let c1width = canvas1.clientWidth;
 
@@ -95,42 +105,71 @@ export function getCovid(){
         canvas1.height = c1height;
 
         let ctx = canvas1.getContext("2d");
-        ctx.fillStyle = "#ffffff";
-        ctx.fillRect(0, 0, c1width, c1height);
+
         ctx.beginPath();
         ctx.strokeStyle = "#f4f4f4";
-        
         //drawing vertical grid
         for(let i = 1; i < days; i++){
             ctx.moveTo(Math.round(i/days*c1width), 0);
             ctx.lineTo(Math.round(i/days*c1width), c1height);
             ctx.stroke();
         }
+        
         //drawing horizontal grid
         for(let i = 1; i <= 10; i++){
             ctx.moveTo(0, 0.1*i*c1height);
             ctx.lineTo(c1width, 0.1*i*c1height);
             ctx.stroke();
         }
-
+        
         ctx.beginPath();
         ctx.strokeStyle = "#000000";
-
+        ctx.lineWidth = 2;
         //drawing the actual graph
-        for(let i = 0; i < days-1; i++){
+        for(let i = 0; i < days; i++){
             ctx.moveTo(c1width * i/days,        c1height-((daily[ID_BY][i]/max_BY)*c1height));
             ctx.lineTo(c1width * (i+1)/days,    c1height-((daily[ID_BY][i+1]/max_BY)*c1height));
-            ctx.stroke(); 
         }
+        ctx.stroke(); 
+    //end bavaria
 
+    // Let the drawing begin (germany)
+        let canvas2 = document.getElementById("ger");
+        let c2height = canvas2.clientHeight;
+        let c2width = canvas2.clientWidth;
 
+        //for some reason this is mathing the interal height with the pixels of the canvas
+        canvas2.width = c2width;
+        canvas2.height = c2height;
 
-        /*
-        Skala max *= 1.1 ? 
-        Input for user -> select day
-        offsets for the legend
+        let cty = canvas2.getContext("2d");
+        cty.beginPath();
+        cty.strokeStyle = "#f4f4f4";
+        //drawing vertical grid
+        for(let i = 1; i < days; i++){
+            cty.moveTo(Math.round(i/days*c2width), 0);
+            cty.lineTo(Math.round(i/days*c2width), c2height);
+        }
+        cty.stroke();
 
-        done ? -> Copy it for germany
-        */
-    }
+        //drawing horizontal grid
+        for(let i = 1; i <= 10; i++){
+            cty.moveTo(0, 0.1*i*c2height);
+            cty.lineTo(c2width, 0.1*i*c2height);
+        }
+        cty.stroke();
+        
+
+        cty.beginPath();
+        cty.strokeStyle = "#000000";
+        cty.lineWidth = 2;
+        //drawing the actual graph
+
+        for(let i = 0; i < days; i++){
+            cty.moveTo(c2width * i/days,        c2height-((daily_DE[i]/max_DE)*c2height));
+            cty.lineTo(c2width * (i+1)/days,    c2height-((daily_DE[i+1]/max_DE)*c2height));
+        }
+        cty.stroke(); 
+    //end Germany
+    }      
 }
